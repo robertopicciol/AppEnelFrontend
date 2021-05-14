49a38f6a7780318de566c6ae15f462a0467be096
Roberto <robertopicciol@live.com>
2021-05-14 17:18:11 +0200

    Aggiornamento del 14/05/2021

---
+++ src/main/kotlin/apps/appAnalisiTisDp/model/AnalisiTisDpAnnoPost.kt
@@ -0,0 +1,4 @@
+package com.roby66one.enel.model.analisiTisDp.bo
+
+data class AnalisiTisDpAnnoPost(val type : String, val annoMeseAnalisi : Long, val annoRif : Long) {
+}
---
+++ src/main/kotlin/apps/appAnalisiTisDp/model/AnalisiTisDpInfoBO.kt
@@ -0,0 +1,10 @@
+package com.roby66one.enel.model.analisiTisDp.bo
+
+class AnalisiTisDpInfoBO(val type : String,
+                         val annoMeseAnalisi : List<Long>,
+                         val tensione : List<String>,
+                         val versoEnergia : List<String>,
+                         val anno_rif : List<Long>,
+                         val annoMese_rif : List<Long>
+                         ) {
+}
